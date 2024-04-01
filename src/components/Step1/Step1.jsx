aa3fb3645aa826bdb32a1b1f541c6d79ae924456
onder <onder.bugdaci@gmail.com>
2024-04-01 14:17:35 +0300

    ufak degisiklikler

--- src/components/Step1/Step1.jsx
+++ src/components/Step1/Step1.jsx
@@ -6,16 +6,17 @@ const Step1 = () => {
               <h1 className="font-bold text-4xl text-marine-blue">Personal Info</h1>
               <p className="text-cool-gray">Please provide your name, email address, and phone number.</p>
               <Step1Content name="Name"
-                            placeholder="Enter your name"/>
+                            placeholder="Enter your name"
+                            type="text"/>
 
               <Step1Content name="Email Address"
-                            placeholder="e.g. stephenking@lorem.com"/>
+                            placeholder="e.g. stephenking@lorem.com"
+                            type="email"/>
 
               <Step1Content name="Phone Number"
-                            placeholder="e.g. +1 234 567 890"/>
+                            placeholder="e.g. +1 234 567 890"
+                            type="number"/>
           </div>
-
-
       </>
   )
 }
--- src/components/Step1/Step1Content.jsx
+++ src/components/Step1/Step1Content.jsx
@@ -1,11 +1,15 @@
-const Step1Content = ({name, placeholder}) => {
+const Step1Content = ({name, placeholder,type}) => {
   return(
       <>
           <div className="mt-4">
               <h3 className="text-sm text-marine-blue">{name}</h3>
-              <input className="w-full h-12 border border-light-gray rounded-lg placeholder:pl-3 hover:border-purplish-blue"
-                     type="text"
-                     placeholder={placeholder}/>
+              <input
+                  className="w-full h-12 border border-light-gray rounded-lg placeholder:pl-3 hover:border-purplish-blue peer"
+                  type={type}
+                  placeholder={placeholder}/>
+              <p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                  Please provide a valid {type === "email" ? "email address." : "phone number"}
+              </p>
           </div>
 
       </>
